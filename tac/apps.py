35ea6f1c3e8c07a6571aa619f83a150de12130cb
alaric <alaric@wullerot.ch>
2021-06-22 14:52:01 +0200

    basic django 3.2 support

--- tac/apps.py
+++ tac/apps.py
@@ -1,9 +1,9 @@
-
-
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
 
 class TACConfig(AppConfig):
+
     name = 'tac'
     verbose_name = _('Datenschutz')
+    default_auto_field = 'django.db.models.AutoField'
